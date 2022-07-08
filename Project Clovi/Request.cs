2fc22ed0b8ff87a4985ab439932c6e531b746938
Rus <definitelyrus@gmail.com>
2022-07-09 06:47:07 +0800

    Added access modifiers and other changes..

--- Project Clovi/Request.cs
+++ Project Clovi/Request.cs
@@ -3,14 +3,14 @@
 	public abstract class Request
 	{
 		/// <summary>
-		/// DO NOT USE. This is used to chain back to the parent RequestDirector. It is set automatically when added to a RequestList.
+		/// This is used to chain back to the parent RequestDirector. It is set automatically when added to a RequestList.
 		/// </summary>
-		RequestDirector Parent { get; set; }
+		public RequestDirector Parent { get; internal set; }
 
 		/// <summary>
 		/// The arguments that this Request can receive and process.
 		/// </summary>
-		Object[] Args { get; set; }
+		public Object[] Params { get; set; }
 
 		/// <summary>
 		/// The unique identifier for this command.
@@ -22,18 +22,19 @@
 		/// </summary>
 		/// <param name="ParentArg"></param>
 		/// <param name="IdArg"></param>
-		/// <param name="ArgsArg"></param>
-		Request(RequestDirector ParentArg, String IdArg, Object[] ArgsArg)
+		/// <param name="ParamsArg"></param>
+		public Request(RequestDirector ParentArg, String IdArg, Object[] ParamsArg)
 		{
 			Parent = ParentArg;
 			Id = IdArg;
-			Args = ArgsArg;
+			Params = ParamsArg;
 		}
 
 		/// <summary>
 		/// Executes the custom request command.
 		/// </summary>
-		/// <returns name="This">This Request for method chaining.</returns>
-		public abstract Request Execute();
+		/// <param name="Args">Any arguments required to execute the request.</param>
+		/// <returns>This Request for method chaining.</returns>
+		public abstract Request Execute(Object[]? Args);
 	}
 }
