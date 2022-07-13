9f12b2a908015c8ebc2d855a3fb92d7e999194a2
Rus <definitelyrus@gmail.com>
2022-07-13 10:27:17 +0800

    Create _RequestTemplate.cs

---
+++ Project Clovi/Requests/_RequestTemplate.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Project_Clovi.Requests
+{
+	public class _RequestTemplate : Request
+	{
+		public _RequestTemplate( String IdArg, String? DescriptionArg = null, SlashCommandOptionBuilder[]? ParamsArg = null, Boolean HasDefaultPermission = true, Boolean HasDMPermission = true, GuildPermission? Perms = null, Boolean IsContextSensitiveArg = true) : base(IdArg, DescriptionArg, ParamsArg, HasDefaultPermission, HasDMPermission, Perms)
+		{
+		}
+
+		public override Request Execute(SocketSlashCommand Command)
+		{
+
+			//Code goes here.
+
+
+			//Do not modify unless necessary.
+			//TODO: Add logger call here and have it send a success message.
+
+			return this;
+		}
+	}
+}
