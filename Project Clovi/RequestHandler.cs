6c6270779eeb05e748ba0b6456bb332260ca6fc3
Rus <definitelyrus@gmail.com>
2022-07-13 10:27:15 +0800

    Create RequestHandler.cs

---
+++ Project Clovi/RequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using Discord.WebSocket;
+
+namespace Project_Clovi
+{
+	public class RequestHandler
+	{
+		async Task SlashCommandHandler(SocketSlashCommand Command)
+		{
+			Console.WriteLine($"Running request \"{Command.CommandName}\"...");
+			await Command.RespondAsync($"Running request \"{Command.CommandName}\"...");
+			await Command.RespondAsync($"Latency:"); //CloviCore is unaccessible from outside of CloviHost. How do I fix that? Getter method?
+
+		}
+	}
+}
