b8c8272e02c92d0c72aff16ddc2acb4dfa77ad1a
Rus <definitelyrus@gmail.com>
2022-07-14 06:22:31 +0800

    Update CloviHost.cs

--- Project Clovi/CloviHost.cs
+++ Project Clovi/CloviHost.cs
@@ -11,6 +11,8 @@ public class CloviHost
 	/// </summary>
 	public readonly DiscordSocketClient CloviCore = new();
 
+	public RequestDirector Director = new(new List<Request>());
+
 	public static Task Main() => new CloviHost().MainAsync();
 	#endregion
 
@@ -39,18 +41,20 @@ public class CloviHost
 	{
 		ulong GuildId = 262784778690887680; //!!! TEMPORARY; must be saved on a case-by-case basis.
 		SocketGuild Guild = CloviCore.GetGuild(GuildId);
+		Request[] RequestList = Array.Empty<Request>(); //TEMP
 
-		try
-		{
-			SlashCommandProperties newCmd = new SlashCommandBuilder().WithName("ping").WithDescription("test").Build();
 		//TODO: Retrieve all standard Request library Requests. (i.e. The premade requests.)
 
-			await Guild.CreateApplicationCommandAsync(newCmd);
 		//TODO: Retrieve the array from a JSON file. This contains any custom commands.
 		//NOTE: Do not store Arrays directly in JSONs. A good option is to store an Array as a value in a Dictionary.
 
+		foreach (Request r in RequestList) Director.AddRequestItem(r); //Adds custom commands to RequestDirector.
 
+		foreach (Request r in Director.RequestList)  //Adds all commands to Discord's listener.
+			await Guild.CreateApplicationCommandAsync(r.DiscordCommand);
 
+		try
+		{
 			CloviCore.SlashCommandExecuted += SlashCommandHandler; //Activated when a command is received.
 		}
 		catch (Exception ex)
