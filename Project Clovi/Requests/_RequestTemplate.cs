01029a67337528703ba3ebddc561fc1d17642db7
Rus <definitelyrus@gmail.com>
2022-07-15 16:06:42 +0800

    Added default value

--- Project Clovi/Requests/_RequestTemplate.cs
+++ Project Clovi/Requests/_RequestTemplate.cs
@@ -5,7 +5,7 @@ namespace Project_Clovi.Requests;
 
 public class _RequestTemplate : Request
 {
-	public _RequestTemplate( String IdArg, String? DescriptionArg = null, SlashCommandOptionBuilder[]? ParamsArg = null, Boolean HasDefaultPermission = true, Boolean HasDMPermission = true, GuildPermission? Perms = null, Boolean IsContextSensitiveArg = true) : base(IdArg, DescriptionArg, ParamsArg, HasDefaultPermission, HasDMPermission, Perms)
+	public _RequestTemplate( String IdArg = "TEMPLATE; PLEASE REFACTOR.", String? DescriptionArg = null, SlashCommandOptionBuilder[]? ParamsArg = null, Boolean HasDefaultPermission = true, Boolean HasDMPermission = true, GuildPermission? Perms = null, Boolean IsContextSensitiveArg = true) : base(IdArg, DescriptionArg, ParamsArg, HasDefaultPermission, HasDMPermission, Perms)
 	{
 	}
 
@@ -13,7 +13,6 @@ public class _RequestTemplate : Request
 	{
 
 		//Code goes here.
-
 
 		//Do not modify unless necessary.
 		//TODO: Add logger call here and have it send a success message.
