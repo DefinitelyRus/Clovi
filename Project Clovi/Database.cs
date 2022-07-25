d4fea9cc4211f1550150f9752835da1714547d0f
Rus <definitelyrus@gmail.com>
2022-07-26 06:46:39 +0800

    Update Database.cs

--- Project Clovi/Database.cs
+++ Project Clovi/Database.cs
@@ -8,4 +8,31 @@ public class Database
 	}
 
 	public String Id { get; set; }
+
+	/// <summary>
+	/// An in-memory copy of any active table.
+	/// </summary>
+	internal Dictionary<String, Dictionary<uint, Object>> Tables { get; set; }
+	/*
+	 * Dictionary
+	 * -> Table (#)
+	 * ---> Row (=)
+	 * -----> Column (Single Item; [])
+	 */
+
+	private void RetrieveTable(String TableName)
+	{
+		//Get table from DB file.
+
+		Dictionary<uint, Dictionary<String, Object>> Table = new();
+		Dictionary<String, Object> Row = new();
+
+		//Foreach row in DB...
+			uint index = 0;
+
+			Row.Add("Column Name", "Value");
+
+			//Foreach column...
+			Table.Add(index, Row);
+	}
 }
