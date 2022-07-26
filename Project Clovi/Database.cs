96c09200199bd7a53e92d59277b8841466af6660
Rus <definitelyrus@gmail.com>
2022-07-27 06:28:28 +0800

    a

--- Project Clovi/Database.cs
+++ Project Clovi/Database.cs
@@ -19,20 +19,4 @@ public class Database
 	 * ---> Row (=)
 	 * -----> Column (Single Item; [])
 	 */
-
-	private void RetrieveTable(String TableName)
-	{
-		//Get table from DB file.
-
-		Dictionary<uint, Dictionary<String, Object>> Table = new();
-		Dictionary<String, Object> Row = new();
-
-		//Foreach row in DB...
-			uint index = 0;
-
-			Row.Add("Column Name", "Value");
-
-			//Foreach column...
-			Table.Add(index, Row);
-	}
 }
--- Project Clovi/DatabaseDirector.cs
+++ Project Clovi/DatabaseDirector.cs
@@ -4,35 +4,47 @@ public abstract class DatabaseDirector
 	public DatabaseDirector(string SQLDialect)
 	{
 		Dialect = SQLDialect;
-		DatabaseList = new();
-	}
-
-	List<Database> DatabaseList { get; set; }
-
-	public DatabaseDirector AddDatabase(Database database)
-	{
-		DatabaseList.Add(database);
-		return this;
-	}
-
-	public DatabaseDirector RemoveDatabasae(String Id)
-	{
-		foreach (Database db in DatabaseList)
-			if (db.Id == Id) DatabaseList.Remove(db);
-
-		return this;
 	}
 
 	public String Dialect { get; private set; }
 
 	private LinkedList<String> SQLQueue = new();
 
-	public Database SelectedDatabase { get; private set; }
-
-	public Object? GetByPrimaryKey(String TableName, String ColumnName, String Key)
+	public Object? GetRecord(String Key, String ColumnName, String TableName)
 	{
-
+		//<-- SELECT ColumnName FROM TableName WHERE Id = Key
 		return null;
 	}
 
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="TableName"></param>
+	/// <param name="GroupByRow">Defines which direction the output will be.
+	/// 0 sorts by row, which uses more memory. 1 sorts by column, limiting its use.</param>
+	private void GetTable(String TableName, bool GroupByRow)
+	{
+		//<-- SELECT * FROM TableName
+		//NOTE: This is dialect-specific. How on earth do I make this standard?
+		//TODO: Create an object that contains all the results, regardless of its contents. Then have it retrievable here.
+
+		if (GroupByRow)
+		{
+			//foreach row in table, make a new dictionary, then add all items into dictionary then add dictionary into table (also dict)
+		}
+		else
+		{
+			//foreach column in table, make new list then add all items into list then add list into dictionary
+		}
+	}
+
+	public Dictionary<String, Object> GetRow(String PrimaryKey, String TableName)
+	{
+		Dictionary<String, Object> Row = new();
+		//<-- select * from TableName where id = PrimaryKey
+		//foreach item in row, add item into dict.
+		return Row;
+	}
+
+	public abstract void ExecuteQuery(String SQLCommand);
 }
