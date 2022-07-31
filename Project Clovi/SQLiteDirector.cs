42ef033a22f1035d9c1ecf7a60edf43b82c48ad8
Rus <definitelyrus@gmail.com>
2022-08-01 05:37:08 +0800

    Added comments

--- Project Clovi/SQLiteDirector.cs
+++ Project Clovi/SQLiteDirector.cs
@@ -2,6 +2,9 @@
 
 using Microsoft.Data.Sqlite;
 
+/// <summary>
+/// Handles all transactions between the bot and the database.
+/// </summary>
 public class SQLiteDirector : DatabaseDirector
 {
 	SQLiteDirector(String Name) : base(SQLDialect: "SQLite")
@@ -10,15 +13,40 @@ public class SQLiteDirector : DatabaseDirector
 		DatabaseName = Name;
 	}
 
+	/*
+	 * !!!!!
+	 * It seems I mistakenly combined the director and the database object itself.
+	 * The SqliteConnection object is to be stored in individual SQLiteDatabase objects.
+	 * The execute/query functions are to be stored there too,
+	 * but allow them to be called from this director.
+	 * !!!!!
+	 */
+
+	/// <summary>
+	/// The connection to the SQLite database. Close this connection before exiting the parent program.
+	/// </summary>
 	public SqliteConnection Connection { get; private set; }
 
+	/// <summary>
+	/// The name of this database. Used to identify between databases for logging and debugging purposes.
+	/// </summary>
 	public string DatabaseName { get; private set; }
 
+	/// <summary>
+	/// Sends the command to the database and applies any changes.
+	/// </summary>
+	/// <param name="SQLCommand">The SQL command to be sent to the database.</param>
+	/// <returns>(Integer) The number of rows affected by the command.</returns>
 	public override Object Execute(String SQLCommand)
 	{
 		return new SqliteCommand(SQLCommand, Connection).ExecuteNonQuery();
 	}
 
+	/// <summary>
+	/// Returns the results of an SQL query. Needs to be closed after use.
+	/// </summary>
+	/// <param name="SQLCommand">The SQL command to be sent to the database.</param>
+	/// <returns>Results of the query.</returns>
 	public override SqliteDataReader Query(String SQLCommand)
 	{
 		return new SqliteCommand(SQLCommand, Connection).ExecuteReader();
