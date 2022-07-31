6849f3d3097b444b33eac94c51e1e3a0eae2552b
Rus <definitelyrus@gmail.com>
2022-08-01 05:36:20 +0800

    Created SQLIte implementation of DatabaseDirector.

--- Project Clovi/SQLiteDirector.cs
+++ Project Clovi/SQLiteDirector.cs
@@ -4,9 +4,23 @@ using Microsoft.Data.Sqlite;
 
 public class SQLiteDirector : DatabaseDirector
 {
-	SQLiteDirector() : base(SQLDialect: "SQLite")
+	SQLiteDirector(String Name) : base(SQLDialect: "SQLite")
 	{
-		//write stuff here dammit
-		//put the implementations here. make dbdirector fully abstract or something
+		Connection = new($@"Data Source={CloviHost.FIODirector.Directory[0]}\GuildsData.s3db");
+		DatabaseName = Name;
+	}
+
+	public SqliteConnection Connection { get; private set; }
+
+	public string DatabaseName { get; private set; }
+
+	public override Object Execute(String SQLCommand)
+	{
+		return new SqliteCommand(SQLCommand, Connection).ExecuteNonQuery();
+	}
+
+	public override SqliteDataReader Query(String SQLCommand)
+	{
+		return new SqliteCommand(SQLCommand, Connection).ExecuteReader();
 	}
 }
