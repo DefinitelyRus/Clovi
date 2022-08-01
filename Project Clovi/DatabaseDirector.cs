f244ecff0826ad23fa046f2635f556b7f8ad21a4
Rus <definitelyrus@gmail.com>
2022-08-02 06:16:29 +0800

    Removed unnecessary code.

--- Project Clovi/DatabaseDirector.cs
+++ Project Clovi/DatabaseDirector.cs
@@ -12,31 +12,8 @@ public abstract class DatabaseDirector
 
 	public Object? GetRecord(String Key, String ColumnName, String TableName)
 	{
-		//<-- SELECT ColumnName FROM TableName WHERE Id = Key
-		return null;
 	}
-
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="TableName"></param>
-	/// <param name="GroupByRow">Defines which direction the output will be.
-	/// 0 sorts by row, which uses more memory. 1 sorts by column, limiting its use.</param>
-	private void GetTable(String TableName, bool GroupByRow)
-	{
-		//<-- SELECT * FROM TableName
-		//NOTE: This is dialect-specific. How on earth do I make this standard?
-		//TODO: Create an object that contains all the results, regardless of its contents. Then have it retrievable here.
-
-		if (GroupByRow)
-		{
-			//foreach row in table, make a new dictionary, then add all items into dictionary then add dictionary into table (also dict)
-		}
-		else
-		{
-			//foreach column in table, make new list then add all items into list then add list into dictionary
-		}
-	}
+	public abstract Object? GetRecord(String Key, String ColumnName, String TableName, String DatabaseName);
 
 	public Dictionary<String, Object> GetRow(String PrimaryKey, String TableName)
 	{
