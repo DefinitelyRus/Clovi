ee00c9966ff94c0fdb2aaa2fd0c2f14f4caf217f
Rus <definitelyrus@gmail.com>
2022-08-11 07:31:49 +0800

    New Request for testing

---
+++ Project Clovi/Requests/Ctest.cs
@@ -0,0 +1,45 @@
+﻿namespace Project_Clovi.Requests;
+
+using Discord;
+using Discord.WebSocket;
+
+public class Ctest : Request
+{
+	public Ctest
+		(
+		String IdArg = "ctest",
+		String DescriptionArg = "A debugging tool used for testing new commands.",
+		SlashCommandOptionBuilder[]? ParamsArg = null,
+		Boolean HasDefaultPermission = true,
+		Boolean HasDMPermission = true,
+		GuildPermission? Perms = null
+		)
+
+		: base(IdArg, DescriptionArg, ParamsArg, HasDefaultPermission, HasDMPermission, Perms) { }
+
+	public override Request Execute(SocketSlashCommand Command, DiscordSocketClient Core)
+	{
+		//Optional prerequisites. Delete or modify only if necessary.
+		ConsoleDirector CD = CloviHost.ConDirector; //Use CD.W(_) for logging.
+
+		//Code goes here.
+		CD.W("Resetting database...");
+		try
+		{
+			CloviHost.SQLDirector.ResetDatabase();
+			Command.RespondAsync("Resetting database...");
+		}
+		catch (Exception e)
+		{
+			CD.W(e.ToString());
+			Command.RespondAsync($"```{e.ToString()}```");
+			return this;
+		}
+		CD.W("Finished resetting database.");
+		//---------------
+
+		//Optional post-fixed instructions. Delete or modify only if necessary.
+		CD.W($"SUCCESS: \"{this.Id}\" request by {Command.User.Username}");
+		return this;
+	}
+}
