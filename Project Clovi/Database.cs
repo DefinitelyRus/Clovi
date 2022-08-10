83f633bbc3bb325afd989ae39a0be2773fe757de
Rus <definitelyrus@gmail.com>
2022-08-10 08:57:24 +0800

    Made functions non-nullable

--- Project Clovi/Database.cs
+++ Project Clovi/Database.cs
@@ -17,12 +17,12 @@ public abstract class Database
 	/// </summary>
 	/// <param name="SQLCommand">The SQL command to be sent to the database.</param>
 	/// <returns>(Integer) The number of rows affected by the command.</returns>
-	public abstract Object? Execute(String SQLCommand);
+	public abstract Object Execute(String SQLCommand);
 
 	/// <summary>
 	/// Returns the results of an SQL query. Needs to be closed after use.
 	/// </summary>
 	/// <param name="SQLCommand">The SQL command to be sent to the database.</param>
 	/// <returns>Results of the query.</returns>
-	public abstract Object? Query(String SQLCommand);
+	public abstract Object Query(String SQLCommand);
 }
--- Project Clovi/DatabaseDirector.cs
+++ Project Clovi/DatabaseDirector.cs
@@ -10,7 +10,7 @@ public abstract class DatabaseDirector
 
 	private LinkedList<String> SQLQueue = new();
 
-	public abstract Object? GetRecord(String Key, String ColumnName, String TableName, String DatabaseName);
+	public abstract Object GetRecord(String Key, String ColumnName, String TableName, String DatabaseName);
 
 	public Dictionary<String, Object> GetRow(String PrimaryKey, String TableName)
 	{
@@ -20,7 +20,8 @@ public abstract class DatabaseDirector
 		return Row;
 	}
 
-	public abstract Object? Execute(String DatabaseName, String SQLCommand);
 
-	public abstract Object? Query(String DatabaseName, String SQLCommand);
+	public abstract Object Execute(String DatabaseName, String SQLCommand);
+
+	public abstract Object Query(String DatabaseName, String SQLCommand);
 }
