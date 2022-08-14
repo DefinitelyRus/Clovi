755c154dfd19d8f92aca1af279f465a4d099a5c6
Rus <definitelyrus@gmail.com>
2022-08-14 15:34:09 +0800

    Updated documentation, added aliases, & minor tweaks

--- Project Clovi/ConsoleDirector.cs
+++ Project Clovi/ConsoleDirector.cs
@@ -27,60 +27,53 @@ public class ConsoleDirector
 	/// </summary>
 	private static String TimeNow = "CriticalTimeError";
 
-	internal List<ulong> ChannelIdList = new();
+	/// <summary>
+	/// A list of channel IDs belonging to SocketTextChannels assigned to receive log messages.
+	/// </summary>
+	internal List<ulong> LogChannelIdList = new();
 
+	/// <summary>
+	/// Whether CloviCore is online and can send messages on Discord.
+	/// </summary>
 	internal bool IsOnline { get; set; }
 
+	//NOTE: Might not be necessary at all. Consider for removal.
+	/// <summary>
+	/// Decides if the PendingLog should be sent (false) or not (true).
+	/// </summary>
 	private bool WaitingForQueue { get; set; }
 
+	/// <summary>
+	/// A collection of log messages combined into one string.
+	/// </summary>
 	private StringBuilder PendingLog { get; }
 	#endregion
 
 	#region Methods
 	/// <summary>
-	/// Prints the input string to the console along with a timestamp.
+	/// Prints the input string to the console along with a timestamp, then optionally adds the message to the PendingLog.
 	/// </summary>
 	/// <param name="Text"></param>
-	[Obsolete("This method is for redundancy only. Use ConsoleDirector.W() instead.", false)]
-	public void Print(String Text)
+	[Obsolete("This method is for clarity only. Use ConsoleDirector.P() instead.", false)]
+	public void Print(String Text, bool AddToPendingLog = false)
 	{
 		StackTrace Source = new();
 		TimeNow = $"{DateTime.Now.Hour:00}:{DateTime.Now.Minute:00}:{DateTime.Now.Second:00}";
 		#pragma warning disable CS8602
 		String Output = $"{TimeNow} {Source.GetFrame(3).GetMethod().Name}() >> {Text}";
 		#pragma warning restore CS8602
+
 		Console.WriteLine($"{Output}\n");
-
-		//Prints the log to the target channel.
-		//TODO: Accumulate all failed prints, then send in one go.
-		//Identify where to finally send the logs.
-		if (!IsOnline)
-		{
-			PendingLog.AppendLine(Output);
-		}
-		else
-		{
-			foreach (ulong id in ChannelIdList)
-			{
-				SocketTextChannel Channel = (SocketTextChannel)CloviHost.CloviCore.GetChannel(id);
-
-				//If PendingLog is empty, send Output only.
-				if (PendingLog.Length == 0) Channel.SendMessageAsync($"```{Output}```");
-				else //else, append Output to PendingLog, then clear PendingLog.
-				{
-					Channel.SendMessageAsync($"```{PendingLog}{Output}```");
-					PendingLog.Clear();
-				}
-			}
-		}
+		if (AddToPendingLog) PendingLog.AppendLine(Output);
 	}
 
 	/// <summary>
 	/// Logs the inserted text to the console, target Discord channel(s), and console log files.
+	/// If the bot is not online, the message will be forwarded to ConsoleDirector.Print().
 	/// </summary>
-	/// <param name="Text"></param>
-	/// <param name="IsFinal"></param>
-	[Obsolete("This method is for redundancy only. Use ConsoleDirector.W() instead.", false)]
+	/// <param name="Text">The text to be logged.</param>
+	/// <param name="IsFinal">Whether to also send the message to the destination(s).</param>
+	[Obsolete("This method is for clarity only. Use ConsoleDirector.W() instead.", false)]
 	public void Log(String Text, bool IsFinal = false)
 	{
 		if (!IsOnline) { Print(Text); return; }
@@ -109,7 +102,6 @@ public class ConsoleDirector
 
 					SocketTextChannel Channel = (SocketTextChannel) CloviHost.CloviCore.GetChannel(id);
 
-					//If PendingLog is empty...
 					if (PendingLog.Length == 0) Channel.SendMessageAsync($"```{Output}```");
 					else
 					{
@@ -123,6 +115,9 @@ public class ConsoleDirector
 		catch (Exception e) { Console.WriteLine(e.ToString()); }
 	}
 
+	/// <summary>
+	/// Sends the PendingLog to their destination(s).
+	/// </summary>
 	public void SendLog()
 	{
 		if (PendingLog.Length == 0) return;
@@ -153,11 +148,28 @@ public class ConsoleDirector
 
 	#region Aliases
 	/// <summary>
-	/// An alias for ConsoleDirector.Log(). It simply logs the input string to the console, along with the timestamp and parent method.
+	/// An alias for ConsoleDirector.Log() and functions identically to Q().
+	/// It simply logs the input string to the console, along with the timestamp and parent method.
 	/// </summary>
-	/// <param name="Text"></param>
+	/// <param name="Text">The text you wish to log.</param>
 	#pragma warning disable CS0618
 	public void W(String Text, bool IsFinal = false) => Log(Text, IsFinal);
+
+
+	/// <summary>
+	/// An alias for ConsoleDirector.Log() and functions identically to W().
+	/// It simply logs the input string to the console, along with the timestamp and parent method.
+	/// </summary>
+	/// <param name="Text">The text you wish to log.</param>
+	public void Q(String Text, bool IsFinal = false) => Log(Text, IsFinal);
+
+	/// <summary>
+	/// An alias for ConsoleDirector.Print().
+	/// It simply prints the text to console and optionally adds the text to the pending log.
+	/// </summary>
+	/// <param name="Text"></param>
+	/// <param name="AddToPendingLog"></param>
+	public void P(String Text, bool AddToPendingLog = false) => Print(Text, AddToPendingLog);
 	#pragma warning restore CS0618
 	#endregion
 	#endregion
