1a7579af80e1b7d6534e71292da1a7c8732ad1ec
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2023-04-27 19:24:25 +0800

    Create GetArt.cs
    This will be used later for a new functionality.

---
+++ Project Clovi/Requests/GetArt.cs
@@ -0,0 +1,35 @@
+﻿namespace Project_Clovi.Requests;
+
+using Discord;
+using Discord.WebSocket;
+
+public class GetArt : Request
+{
+	public GetArt
+		(
+		String IdArg = "req-template",
+		String DescriptionArg = "temp description",
+		List<Dictionary<string, object?>>? OptionDictionaryList = null,
+		Boolean HasDefaultPermission = true,
+		Boolean HasDMPermission = true,
+		GuildPermission? Perms = null
+		)
+
+		: base(IdArg, DescriptionArg, OptionDictionaryList, HasDefaultPermission, HasDMPermission, Perms) { }
+
+	public override Request Execute(SocketSlashCommand Command, DiscordSocketClient Core)
+	{
+		//Optional prerequisites. Delete or modify only if necessary.
+		ConsoleDirector CD = CloviHost.ConDirector; //Use CD.W(...) for logging.
+		CD.W($"User {Command.User.Username} used command {this.Name}...");
+		//IMPORTANT: Do not use CD.W("...", true) in this method unless absolutely necessary.
+
+		//Code goes here.
+
+		//---------------
+
+		//Optional post-fixed instructions. Delete or modify only if necessary.
+		CD.W($"SUCCESS: \"{this.Name}\" request by {Command.User.Username}");
+		return this;
+	}
+}
